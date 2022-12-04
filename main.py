5e3e566b5d86164e197d0b709b1b4390de06b916
Mohamed Salat <89856630+JustGetAClass@users.noreply.github.com>
2022-12-04 16:50:14 +0300

    Final commit

--- main.py
+++ main.py
@@ -10,27 +10,27 @@ entry = Entry(width=10)
 entry.grid(row=0, column=1)
 
 # Label 1
-label_1 = Label(text="Miles")
-label_1.grid(row=0, column=2)
+label_miles = Label(text="Miles")
+label_miles.grid(row=0, column=2)
 
 # Label 2
-label_2 = Label(text="is Equal to")
-label_2.grid(row=1, column=0)
+label_equal = Label(text="is Equal to")
+label_equal.grid(row=1, column=0)
 
 # Label 3
-label_3 = Label(text="0")
-label_3.grid(row=1, column=1)
+label_convert = Label(text="0")
+label_convert.grid(row=1, column=1)
 
 # Label 4
-label_4 = Label(text="Km")
-label_4.grid(row=1, column=2)
+label_kilometer = Label(text="Km")
+label_kilometer.grid(row=1, column=2)
 
 
 # Button
 def covert_miles():
     miles = int(entry.get())
     km = round(miles * 1.609, 1)
-    label_3.config(text=str(km))
+    label_convert.config(text=str(km))
 
 
 button = Button(text="Calculate", command=covert_miles)
